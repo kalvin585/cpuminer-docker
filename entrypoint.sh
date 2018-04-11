799a6a81dcb1f02bb1fc156c92c38866215023e3
Kalvin Harris <harriskalvin585@gmail.com>
2018-04-11 12:09:49 +0700

    New upstream version. Added cpuflags variable to override /proc/cpuinfo while launching.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,38 @@
 #!/bin/sh
+test -z "$cpuflags" && cpuflags=$(cat /proc/cpuinfo)
 #checking AES+AVX2 support
-if [ -n "$(cat /proc/cpuinfo | grep avx2)" ]; then
+if [ -n "$(echo "$cpuflags " | grep 'avx2 ')" ]; then
 	echo Starting CPU miner with AVX2 support
 	/usr/local/bin/cpuminer-avx2 $@
 else
 # checking AES+AVX support
-if [ -n "$(cat /proc/cpuinfo | grep aes | grep avx)" ]; then
+if [ -n "$(echo "$cpuflags " | grep 'aes ' | grep 'avx ')" ]; then
 	echo Starting CPU miner with AES+AVX support
 	/usr/local/bin/cpuminer-aes-avx $@
 else
 # checking AES+SSE4.2 support
-if [ -n "$(cat /proc/cpuinfo | grep aes | grep sse4_2)" ]; then
+if [ -n "$(echo "$cpuflags " | grep 'aes ' | grep 'sse4_2 ')" ]; then
 	echo Starting CPU miner with AES+SSE4_2 support
 	/usr/local/bin/cpuminer-aes-sse42 $@
 else
 # checking SSE4.2 support
-if [ -n "$(cat /proc/cpuinfo | grep sse4_2)" ]; then
+if [ -n "$(echo "$cpuflags " | grep 'sse4_2 ')" ]; then
 	echo Starting CPU miner with SSE4_2 support
 	/usr/local/bin/cpuminer-sse42 $@
 else
+# checking SSSE3 support
+if [ -n "$(echo "$cpuflags " | grep 'ssse3 ')" ]; then
+	echo Starting CPU miner with SSSE3 support
+	/usr/local/bin/cpuminer-ssse3 $@
+else
 # checking SSE2 support
-if [ -n "$(cat /proc/cpuinfo | grep sse2)" ]; then
+if [ -n "$(echo "$cpuflags " | grep 'sse2 ')" ]; then
 	echo Starting CPU miner with SSE2 support
 	/usr/local/bin/cpuminer-sse2 $@
 else
 # no features found
 	echo No suitable CPU features found, there is nothing to launch
+fi
 fi
 fi
 fi
